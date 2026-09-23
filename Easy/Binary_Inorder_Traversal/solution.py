bd8fc473af9e6809797b3bccb01f60663712c3fe
sachithdh <sachithdhanushka00@gmail.com>
2026-09-23 17:39:49 +0530

    Add solution for Binary Inorder Traversal problem

---
+++ Easy/Binary_Inorder_Traversal/solution.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: TreeNode | None) -> list[int]:
+        if root == None:
+            return []
+        l = []
+        if root.left:
+            l += self.inorderTraversal(root.left)
+        l.append(root.val)
+        
+        if root.right:
+                l += self.inorderTraversal(root.right)
+        return l
